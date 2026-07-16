8c97ee3df381edf90b72b9f86e4fe1da85848406
Biscuit3838 <testelinmaxime@gmail.com>
2026-07-16 19:37:38 +0000

    Explosions et mises de 15

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,31 +6,45 @@ st.write(
     "Tu entres un nombre. Tu valides. Tu cliques sur Calculer. Tu pleures parce que tu as min maxé et que tu as 0 mises."
 )
 number_dices = st.number_input("Nombre de dés", step=1)
-st.divider()
+explosion = st.checkbox("10 Explosent")
+mises_Q = st.checkbox("Mises de 15")
 
 
 def calculs(num_dices) :
     l = []
     for i in range(num_dices):
         l.append(r.randint(1, 10))
+    l_ori = l.copy()
+    l_reroll = []
+    mises = 0
+    if not mises_Q :
+        k = 0
+        while k < len(k) :
+            if l[k] == 10 :
+                if explosion :
+                    while l[k] == 10 :
+                        mises += 1
+                        l[k] = r.randint(1, 10)
+                        l_reroll.append(l[k])
+                else :
+                    mises += 1
+                    l.pop(k)
+                    k -= 1
+            k += 1
     l.sort()
     l.reverse()
-    l_ori = l.copy()
-    mises = 0
-    j = 0
-    while j < len(l) :
-        if l[j] == 10 :
-            l.pop(j)
-            mises += 1
-            j -= 1
-        j += 1
-    j = 0
-    while j < 9:
-        retour = calculs_start(l, 10 + j)
+    if mises_Q :
+        j = 15
+        limit = 21
+    else :
+        j = 10
+        limit = 18
+    while j < limit:
+        retour = calculs_start(l, j)
         mises += retour[0]
         l = retour[1]
         j += 1
-    return mises, l_ori, l
+    return mises, l_ori, l, l_reroll
 
 def calculs_start(l, limit) :
     mises = 0
@@ -78,6 +92,7 @@ if st.button("Calculer"):
     mises = retour[0]
     liste = retour[1]
     liste_rest = retour[2]
+    liste_reroll = retour[3]
     des = ""
     des_rest = ""
     for d in liste:
@@ -86,5 +101,13 @@ if st.button("Calculer"):
         des_rest = des_rest + str(d_r) + " "
     st.divider()
     st.write("Dés : ", des)
-    st.write("Dés restants :", des_rest)
-    st.write("Mises : ", mises)
+    st.write("Dés restants : ", des_rest)
+    if explosion :
+        des_roll = ""
+        for d_roll in liste_reroll :
+            des_roll = des_roll + str(d_roll) + " "
+        st.write("Dés rerolls : ", des_roll)
+    if mises_Q :
+        st.write("Mises : ", mises, " x 2 = ", mises * 2)
+    else :
+        st.write("Mises : ", mises)
