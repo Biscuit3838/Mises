8d3c793acc0178b1640de47a90635d68e2735126
Biscuit3838 <testelinmaxime@gmail.com>
2026-07-16 15:51:24 +0000

    TestNum

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("MISES WOOOOOOO")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+number = st.number_input("Nombre de dés")
+st.write("Current num : " + number)
