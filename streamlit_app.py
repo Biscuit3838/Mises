e7a1b9d00088500b2d16a3cb44d552819a250227
Biscuit3838 <testelinmaxime@gmail.com>
2026-07-16 15:53:59 +0000

    Rien

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 number = st.number_input("Nombre de dés")
-st.write("Current num : " + number)
+st.write("Current num : ", number)
